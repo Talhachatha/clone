d226d89d8ce36591f2937b5e4bf218820cb0bd41
ilsa <Talha.jaleel@ilsainteractive.com>
2021-11-30 18:56:18 +0500

    new2

--- groups/views.py
+++ groups/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.mixins import(
     PermissionRequiredMixin
 )
 
+#gdfdfd
 from django.urls import reverse
 from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
